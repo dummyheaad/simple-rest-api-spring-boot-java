b5ae56574671a251a9ea6fd8225db0c625b0d716
Aysuka Ansari <aysukaansari@apps.ipb.ac.id>
2023-10-02 12:05:33 +0700

    first commit

---
+++ src/main/java/com/example/demo/model/Tutorial.java
@@ -0,0 +1,58 @@
+package com.example.demo.model;
+
+public class Tutorial {
+    private long id = 0;
+
+    private String title;
+
+    private String description;
+
+    private boolean published;
+
+    public Tutorial() {
+
+    }
+
+    public Tutorial(String title, String description, boolean published) {
+        this.title = title;
+        this.description = description;
+        this.published = published;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPublished() {
+        return this.published;
+    }
+
+    public void setPublished(boolean isPublished) {
+        this.published = isPublished;
+    }
+
+    @Override
+    public String toString() {
+        return "Tutorial [id=" + this.id + ", title=" + this.title + ", desc=" + this.description + ", published=" + this.published + "]";
+    }
+}
